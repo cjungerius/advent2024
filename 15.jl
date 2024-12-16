14013bb3edbf5df9c29cfeb32abfffa8df50e529
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2024-12-16 17:25:02 +0100

    solve day 15 p2

--- 15.jl
+++ 15.jl
@@ -1,4 +1,4 @@
-# Day 15 solution
+# Day 15 solution: 16.379 ms (146321 allocations: 5.05 MiB)
 
 input = readlines("input/15_input.txt")
 
@@ -31,13 +31,10 @@ function day_fifteen(input)
         return next_x, next_y
     end
     
-    function test_move_2(x,y,dir)
-        cur = bigger_env[y,x]        
+    function test_move_2(x,y,dir)      
         next_x, next_y = (x, y) .+ dirs[dir]
-        next = bigger_env[next_y,next_x]
-        self_free = false
-        neighbor_free = false
-        if cur == 3
+        next = bigger_env[next_y, next_x]
+        if dir == '<' || dir == '>'
             if next == 0
                 return true
             elseif 0 < next < 3
@@ -45,148 +42,50 @@ function day_fifteen(input)
             else
                 return false
             end
-
-        elseif cur == 1
-            if dir == '>'
-                return test_move_2(next_x, next_y, dir)
-            elseif dir == '<'
-                if next == 0
-                    return true
-                elseif next == 2
-                    return test_move_2(next_x, next_y, dir)
-                else
-                    return false
-                end
+        elseif dir == '^' || dir == 'v'
+            if next == 0
+                return true
+            elseif next == 1
+                return test_move_2(next_x, next_y, dir) && test_move_2(next_x+1, next_y, dir)
+            elseif next == 2
+                return test_move_2(next_x, next_y, dir) && test_move_2(next_x-1, next_y, dir)
             else
-                if next == 0
-                    self_free = true
-                elseif 0 < next < 3
-                    println(bigger_env[next_y,next_x])
-                    self_free = test_move_2(next_x, next_y, dir)
-                else
-                    self_free = false
-                end
-
-                if bigger_env[next_y,next_x+1] == 0
-                    neighbor_free == true
-                elseif 0 < bigger_env[next_y,x+1] < 3
-                    neighbor_free = test_move_2(next_x+1,next_y, dir)
-                else
-                    neighbor_free = false
-                end
-                println(cur, self_free, " ", neighbor_free)
-                return self_free &&  neighbor_free
+                return false
             end
-
-        elseif cur == 2
-            if dir == '<'
-                return test_move_2(next_x, next_y, dir)
-            elseif dir == '>'
-                if next == 0
-                    return true
-                elseif next == 1
-                    return test_move_2(next_x, next_y, dir)
-                else
-                    return false
-                end
-            else
-                if next == 0
-                    self_free = true
-                elseif next == 1 || next == 2
-                    self_free = test_move_2(next_x, next_y, dir)
-                else
-                    self_free = false
-                end
-
-                if bigger_env[next_y,next_x-1] == 0
-                    neighbor_free == true
-                elseif 0 < bigger_env[next_y,x-1] < 3
-                    neighbor_free = test_move_2(x-1,next_y, dir)
-                else
-                    neighbor_free = false
-                end
-
-                return self_free && neighbor_free
-            end
-        end
-        return false
+        end      
     end
 
     function execute_move_2(x,y,dir)
-        cur = bigger_env[y,x]
         next_x, next_y = (x, y) .+ dirs[dir]
+        cur = bigger_env[y, x]
         next = bigger_env[next_y,next_x]
-        if cur == 3
+
+        if dir == '<' || dir == '>'
             if next == 0
                 bigger_env[next_y, next_x] = cur
-                bigger_env[y,x] = 0
+                bigger_env[y, x] = 0
             elseif 0 < next < 3
                 execute_move_2(next_x, next_y, dir)
-                bigger_env[next_y, next_x] = cur
+                bigger_env[next_y,next_x] = cur
                 bigger_env[y,x] = 0
             end
 
-        elseif cur == 1
-            if dir == '>'
+        elseif dir == '^' || dir == 'v'
+            if next == 0
+                bigger_env[next_y, next_x] = cur
+                bigger_env[y, x] = 0
+            elseif next == 1
                 execute_move_2(next_x, next_y, dir)
-                bigger_env[next_y, next_x] = cur
-                bigger_env[y,x] = 0
-            elseif dir == '<'
-                if next == 0
-                    bigger_env[next_y, next_x] = cur
-                    bigger_env[y,x] = 0
-                elseif next == 2
-                    execute_move_2(next_x, next_y, dir)
-                    bigger_env[next_y, next_x] = cur
-                    bigger_env[y,x] = 0
-                end
-            else
-                if next == 0
-                    bigger_env[next_y,next_x] = cur
-                    bigger_env[y,x] = 0
-                elseif 0 < next < 3
-                    execute_move_2(next_x, next_y, dir)
-                    bigger_env[next_y, next_x] = cur
-                    bigger_env[y,x] = 0
-                end
-
-                if bigger_env[next_y,next_x+1] == 0
-                    bigger_env[next_y, next_x+1] = bigger_env[y,x+1]
-                    bigger_env[y, x+1] = 0
-                elseif 0 < bigger_env[next_y,next_x+1] < 3
-                    execute_move_2(next_y,next_x+1, dir)
-                    bigger_env[next_y,next_x+1] = bigger_env[y,x+1]
-                    bigger_env[y,x+1] = 0
-                end
-            end
-
-        elseif cur == 2
-            if dir == '<'
+                execute_move_2(next_x+1,next_y,dir)
+                bigger_env[next_y,next_x] = cur
+                bigger_env[y, x] = 0
+            elseif next == 2
                 execute_move_2(next_x, next_y, dir)
-                bigger_env[next_y, next_x] = cur
-                bigger_env[y,x] = 0
-            elseif dir == '>'
-                if next == 0
-                    bigger_env[next_y, next_x] = cur
-                    bigger_env[y,x] = 0
-                elseif next == 2
-                    execute_move_2(next_x, next_y, dir)
-                    bigger_env[next_y, next_x] = cur
-                    bigger_env[y,x] = 0
-                end
-            else
-                if bigger_env[next_y,next_x-1] == 0
-                    bigger_env[next_y, next_x-1] = bigger_env[y,x-1]
-                    bigger_env[y, x-1] = 0
-                elseif 0 < bigger_env[next_y,next_x-1] < 3
-                    execute_move_2(next_y,next_x-1, dir)
-                    bigger_env[next_y,next_x+1] = bigger_env[y,x-1]
-                    bigger_env[y,x-1] = 0
-                end
+                execute_move_2(next_x-1,next_y,dir)
+                bigger_env[next_y,next_x] = cur
+                bigger_env[y, x] = 0
             end
-        end
-
-
+        end      
         return next_x, next_y
     end
 
@@ -224,22 +123,26 @@ function day_fifteen(input)
     end
 
     n = 0
-    for command in [commands[1]]
-        for c in command[1:22]
-            println(n)
-            println(c)
+    for command in commands
+        for c in command
             n+= 1
             if test_move(robot_x,robot_y,c)
                 robot_x, robot_y = execute_move(robot_x,robot_y,c)
             end
-            if @show test_move_2(robot_x_bigger, robot_y_bigger, c)                
+            if test_move_2(robot_x_bigger, robot_y_bigger, c)                
                 robot_x_bigger, robot_y_bigger = execute_move_2(robot_x_bigger, robot_y_bigger, c)
             end
-            display(colorview(Gray,bigger_env./4))
         end
     end
 
     boxes = findall(==(1), env)
+    bigboxes = findall(==(1), bigger_env)
     
-    sum([100*(x[1]-1) + x[2]-1 for x in boxes])
+    part_one = sum([100*(x[1]-1) + x[2]-1 for x in boxes])
+    part_two = sum([100*(x[1]-1) + x[2]-1 for x in bigboxes])
+    part_one, part_two
 end
+
+part_one, part_two = day_fifteen(input)
+println("Part One: ", part_one)
+println("Part Two: ", part_two)
