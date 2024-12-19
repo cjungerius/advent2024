9f8ef94747e518b45dfb08777e7dbcf9da9572c7
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2024-12-19 16:46:50 +0100

    tweak & add towel filter to greatly speed up search

--- 19.jl
+++ 19.jl
@@ -1,31 +1,41 @@
-# Day 19 solution: 2.635 s (8432918 allocations: 198.37 MiB)
-using Memoization
-
+# Day 19 solution: 481.241 ms (8815674 allocations: 411.64 MiB)
 input = readlines("input/19_input.txt")
 
 function day_nineteen(input)
-    towels = split(input[1],", ")
+    towels = split(input[1], ", ")
     part_one = 0
     part_two = 0
+    memo = Dict{String,Int}()
+    used_towels = String[]
 
-    @memoize function check_design(i, design, n=0)
-        i > length(design) && return 0
-        i == length(design) && return 1
-        for t in towels
-            next_i = i + length(t)
-            if next_i <= length(design) && t == design[i+1:next_i] 
-                n += check_design(next_i, design)
+    function check_design(attempt, design)
+
+        return get!(memo, attempt) do
+
+            n = 0
+            attempt == design && return 1
+
+            for t in used_towels
+                next_attempt = attempt * t
+                if length(next_attempt) <= length(design) && next_attempt == design[1:length(next_attempt)]
+                    n += check_design(next_attempt, design)
+                end
             end
+
+            return n
         end
-        return n
     end
 
     for design in input[3:end]
-       n = check_design(0,design)
-       n > 0 && (part_one += 1)
-       part_two += n
+        used_towels = filter(x -> occursin(x, design), towels)
+        empty!(memo)
+        n = check_design("", design)
+        n > 0 && (part_one += 1)
+        part_two += n
     end
     part_one, part_two
 end
 
-
+part_one, part_two = day_nineteen(input)
+println("Part One: ", part_one)
+println("Part Two: ", part_two)
